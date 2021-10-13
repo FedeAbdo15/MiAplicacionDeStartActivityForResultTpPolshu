a62f85be48b0ce0829dc39f160ab68c11bd5eb8d
devandroid <federicoabdo04@gmail.com>
2021-10-13 09:05:22 -0300

    nasheeeeeeeeeeee

--- app/src/main/java/com/example/miaplicaciondestartactivityforresulttppolshu/MainActivity.java
+++ app/src/main/java/com/example/miaplicaciondestartactivityforresulttppolshu/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.miaplicaciondestartactivityforresulttppolshu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CODIGO_PARA_LA_FOTO = 69;
+    public static final int CODIGO_PARA_EL_AMIGO = 420;
 
     Button btnTomarFotografias;
     Button btnPresentarAmigo;
@@ -23,7 +32,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iniciarReferenciados();
+        setearEscuchadores();
+    }
 
+    private void setearEscuchadores() {
+        btnTomarFotografias.setOnClickListener(v -> {
+            Intent intentDeLaCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intentDeLaCamara, CODIGO_PARA_LA_FOTO);
+        });
+        btnPresentarAmigo.setOnClickListener(v -> {
+            Intent intentDelContacto = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intentDelContacto, CODIGO_PARA_EL_AMIGO);
+
+        });
+        btnPonerTuNombre.setOnClickListener(v -> {
+            Intent intentDeMiNombre = new Intent(this, MainActivity2LaRevancha.class);
+            startActivity(intentDeMiNombre);
+        });
+    }
+
+    void mostrarNombre(String nombre) {
+        tvCosaDeContacto.setText(nombre);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        if (resultCode == RESULT_CANCELED) return;
+
+        switch (reqCode) {
+            case CODIGO_PARA_LA_FOTO:
+                Bitmap picture = (Bitmap) data.getExtras().get("data");
+                imgTuPlasmion.setImageBitmap(picture);
+                break;
+            case CODIGO_PARA_EL_AMIGO:
+                Uri uri = data.getData();
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                cursor.moveToFirst();
+                String nombre = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                mostrarNombre(nombre);
+                break;
+        }
     }
 
     private void iniciarReferenciados() {
--- app/src/main/java/com/example/miaplicaciondestartactivityforresulttppolshu/MainActivity2LaRevancha.java
+++ app/src/main/java/com/example/miaplicaciondestartactivityforresulttppolshu/MainActivity2LaRevancha.java
@@ -2,6 +2,7 @@ package com.example.miaplicaciondestartactivityforresulttppolshu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,14 +20,20 @@ public class MainActivity2LaRevancha extends AppCompatActivity {
         setContentView(R.layout.activity_main_activity2_la_revancha);
 
         inicarRevancha();
+        escuchadoresDeLaRevancha();
+    }
 
-
+    private void escuchadoresDeLaRevancha() {
+        btnNosFuimos.setOnClickListener(v -> {
+            Intent intentYaIngreseElNombrePa = new Intent(this, MainActivity.class);
+            String miNombre = edtNombreDelAmigazoIngresado.getText().toString();
+            intentYaIngreseElNombrePa.putExtra("nombre del amigazo", miNombre);
+        });
     }
 
     private void inicarRevancha() {
         tvTuNombreMiAmor = findViewById(R.id.tvTuNombreMiAmor);
         edtNombreDelAmigazoIngresado = findViewById(R.id.edtNombreDelAmigazoIngresado);
         btnNosFuimos = findViewById(R.id.btnNosFuimos);
-
     }
 }
